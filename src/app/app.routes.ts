d3903169e1b2c4206990f0c0f5017120d5e9ad67
Vitor de Oliveira <vitorturmac@gmail.com>
2026-03-31 19:44:12 -0300

    chore: angular material added and router configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TransferForm } from './components/transfer-form/transfer-form';
+import { TransferList } from './components/transfer-list/transfer-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'form' },
+  { path: 'form', component: TransferForm },
+  { path: 'list', component: TransferList },
+  { path: '**', redirectTo: 'form' }
+];
