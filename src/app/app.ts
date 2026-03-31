702ebf17ab0bc5c3abf21bbb1e17f6c22a2b8085
Vitor de Oliveira <vitorturmac@gmail.com>
2026-03-31 20:21:34 -0300

    feat: responsive navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  menuOpen = false;
   protected readonly title = signal('transfer-scheduler-app');
 }
